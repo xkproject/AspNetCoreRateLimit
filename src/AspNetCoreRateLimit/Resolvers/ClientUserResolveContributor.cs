a1f551ab9db2d1237a445847d9a516c2336e2402
jag84 <javieranyon@pccom.es>
2022-08-19 14:56:30 +0200

    Client Custom Resolver WIP

---
+++ src/AspNetCoreRateLimit/Resolvers/ClientUserResolveContributor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreRateLimit
+{
+    public class ClientUserResolveContributor : IClientResolveContributor
+    {
+        public Task<string> ResolveClientAsync(HttpContext httpContext)
+        {
+            //TO-DO Get user from httpContext and then get ClientId
+            throw new NotImplementedException();
+        }
+    }
+}
